f9213932bf363f7bea920b01bdb649d8c1f9bd56
LE DUC TAM <giaythuytinh176@gmail.com>
2021-03-23 17:28:38 +0700

    validation for filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -14,14 +14,14 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
-import {TaskStatusValidationPipe} from "./pipes/task-status-validation.pipe";
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
     if (Object.keys(filterDto).length) {
       return this.tasksService.getTasksWithFilters(filterDto);
     } else {
