991d954bca516876b5869a18ba8336fec4352f6f
LE DUC TAM <giaythuytinh176@gmail.com>
2021-03-24 01:20:19 +0700

    get a task by id persistence

--- src1/tasks/task.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Task } from './task.entity';
-import { EntityRepository, Repository } from 'typeorm';
-
-@EntityRepository(Task)
-export class TaskRepository extends Repository<Task> {
-
-}
